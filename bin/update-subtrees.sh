fb31af13101c7f414623ff0c6d17f5483469dc34
Andreas Sundqvist <andreas@stefna.is>
2021-07-09 10:28:24 +0200

    Update subtree list

--- bin/update-subtrees.sh
+++ bin/update-subtrees.sh
@@ -3,10 +3,10 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 
 declare -A repositories=(
 	["packages/core"]="secrets-manager-core"
-)
-
-declare -A branches=(
-	["packages/core"]="secrets-manager-core"
+	['packages/json-provider']="secrets-manager-json-provider"
+	['packages/psr-6-provider']="secrets-manager-psr6-provider"
+	['packages/aws-parameter-store-provider']="secrets-manager-aws-ssm-provider"
+	['packages/aws-secrets-manager-provider']="secrets-manager-aws-secrets-manager-provider"
 )
 
 mainBranch="main"
@@ -16,16 +16,14 @@ currentBranch=$(git rev-parse --abbrev-ref HEAD)
 for repoPath in "${!repositories[@]}" ; do
 	echo "Path: $repoPath"
 	echo "Repo: ${repositories[$repoPath]}"
-	echo "Branch: ${branches[$repoPath]}"
 
 	repoName=${repositories[$repoPath]}
-	repoBranch=${branches[$repoPath]}
 
 	git checkout "$mainBranch"
 
-	$DIR/splitsh-lite --prefix="$repoPath" --target=heads/"$repoBranch"
-	git remote add "$repoBranch" "git@github.com:stefna/$repoName.git"
-	git checkout heads/"$repoBranch"
-	git push "$repoBranch" HEAD:main
+	$DIR/splitsh-lite --prefix="$repoPath" --target=heads/"$repoName"
+	git remote add "$repoName" "git@github.com:stefna/$repoName.git"
+	git checkout heads/"$repoName"
+	git push "$repoName" HEAD:"$mainBranch"
 done
 git checkout "$currentBranch"
